74892158a54d1e1b4a58f88644031bcbe0c72b15
dator00 <75337886+dator00@users.noreply.github.com>
2023-09-21 10:42:53 -0500

    Finished video, fixing bug where the list of bookmarks would not display properly.

--- contentScript.js
+++ contentScript.js
@@ -10,12 +10,27 @@
         if (type === "NEW") {
             currentVideo = videoId;
             newVideoLoaded();
+        } else if (type === "PLAY"){
+            youtubePlayer.currentTime = value;
+        } else if (type === "DELETE") {
+            currentVideoBookmarks = currentVideoBookmarks.filter((b) => b.time != value);
+            chrome.storage.sync.set({[currentVideo] :JSON.stringify(currentVideoBookmarks)});
+
+            response(currentVideoBookmarks);
         }
     });
 
-    const newVideoLoaded = () => {
+    const fetchBookmarks = () => {
+        return new Promise((resolve) => {
+            chrome.storage.sync.get([currentVideo], (obj) => {
+                resolve(obj[currentVideo] ? JSON.parse(obj[currentVideo]): []);
+            })
+        })
+    }
+
+    const newVideoLoaded = async () => {
         const bookmarkBtnExists = document.getElementsByClassName("bookmark-btn")[0];
-        console.log(bookmarkBtnExists);
+        currentVideoBookmarks = await fetchBookmarks();
 
         if (!bookmarkBtnExists) {
             const bookmarkBtn = document.createElement("img");
@@ -32,13 +47,14 @@
         }
     }
 
-    const addNewBookmarkEventHandler = () => {
+    const addNewBookmarkEventHandler = async () => {
         const currentTime = youtubePlayer.currentTime;
         const newBookmark = {
             time: currentTime,
             desc: "Bookmark at " + getTime(currentTime),
         };
-        console.log(newBookmark);
+        
+        currentVideoBookmarks = await fetchBookmarks();
 
         chrome.storage.sync.set({
             [currentVideo]: JSON.stringify([...currentVideoBookmarks, newBookmark].sort((a, b) => a.time - b.time))
